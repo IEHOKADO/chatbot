f7ade7d654ec9ae30326bd72d950de95ef88c4ab
IEHOKADO <58584836+IEHOKADO@users.noreply.github.com>
2020-11-26 21:36:27 +0900

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-PATH2QUESTION = 'data/text/question'
-PATH2ANSWER = 'data/text/answer/answer.txt'
-PATH2DICT = ['data/json/char2idx.json', 'data/json/idx2char.json']
-PATH2MODEL = 'data/model/model.h5'
